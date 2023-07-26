6a1033a153eb1b70c7723a10889ff7d38de4c68c
diaa zaytouni <diaazeitouni@gmail.com>
2023-07-26 17:54:54 +0300

    push1

---
+++ src/dashboard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./navbar";
+
+export default function Dashboard() {
+  <div>
+    <Navbar username="bilal" />
+
+    <div class="container">
+      <div class="row">
+        <div class="col">Column</div>
+        <div class="col">Column</div>
+        <div class="col">Column</div>
+      </div>
+    </div>
+  </div>;
+}
